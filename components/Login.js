007db7a63edfa7e86179eb12f631c1db25e70937
Nafis Rayan <nafisrayan123@gmail.com>
2024-04-15 12:43:16 +0600

    login almost done

--- components/Login.js
+++ components/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -8,28 +8,60 @@ import {
   Dimensions,
   Image,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch("https://electronerp.com/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Login failed");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      // Handle successful login here, e.g., navigate to the profile page
+      Alert.alert("Success", "Login successful");
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Error", "Login failed");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image source={require("../assets/app_logo.png")} style={styles.logo} />
       <Text style={styles.title}>Login</Text>
       <TextInput
-        placeholder="Username"
+        placeholder="Email"
         style={styles.input}
         placeholderTextColor="#999"
+        onChangeText={setEmail}
+        value={email}
       />
       <TextInput
         placeholder="Password"
         secureTextEntry
         style={styles.input}
         placeholderTextColor="#999"
+        onChangeText={setPassword}
+        value={password}
       />
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => console.log("Login Pressed")}
-      >
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
     </View>
@@ -39,16 +71,13 @@ const Login = () => {
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-// Calculate responsive padding
-// const windowWidth * 0.05 = windowWidth * 0.05; // 5% of screen width
-
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: windowWidth * 1, // Use responsive padding
-    paddingVertical: windowHeight * 1,
+    paddingHorizontal: windowWidth * 0.1, // Use responsive padding
+    paddingVertical: windowHeight * 0.1,
     backgroundColor: "#F5F5F5",
   },
   title: {
